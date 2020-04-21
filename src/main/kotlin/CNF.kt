5814cd5a401d639c82ed867f0f4fd375feaf4411
Soeking <great.from.g@gmail.com>
2020-04-21 16:36:55 +0900

    fix method

--- src/main/kotlin/CNF.kt
+++ src/main/kotlin/CNF.kt
@@ -24,37 +24,39 @@ class CNF(val size: Int, val clauses: MutableList<Clause>, val literal: MutableL
     }
 
     fun oneLiteral(): CNF {
-        val c = clauses.find { it.element.size == 1 }?.element?.first()
+        val cnf = this
+        val c = cnf.clauses.find { it.element.size == 1 }?.element?.first()
         if (c != null) {
-            clauses.removeAll { it.element.contains(c) }
-            clauses.filter { it.element.contains(-c) }.forEach {
+            cnf.clauses.removeAll { it.element.contains(c) }
+            cnf.clauses.filter { it.element.contains(-c) }.forEach {
                 it.element.remove(-c)
             }
-            if (c > 0) literal[c - 1] = Triple(c, second = true, third = true)
-            if (c < 0) literal[abs(c) - 1] = Triple(-c, second = false, third = true)
-            return this.oneLiteral()
+            if (c > 0) cnf.literal[c - 1] = Triple(c, second = true, third = true)
+            if (c < 0) cnf.literal[abs(c) - 1] = Triple(-c, second = false, third = true)
+            return cnf.oneLiteral()
         }
 
-        return this
+        return cnf
     }
 
     fun pureLiteral(): CNF {
+        val cnf = this
         val l = mutableListOf<Triple<Int, Boolean, Boolean>>()
         repeat(size) {
             l.add(Triple(it + 1, second = false, third = false))
         }
-        clauses.forEach { c ->
+        cnf.clauses.forEach { c ->
             c.element.forEach { e ->
                 if (e > 0) l[e - 1] = Triple(l[e - 1].first, true, l[e - 1].third)
                 else l[abs(e) - 1] = Triple(l[abs(e) - 1].first, l[abs(e) - 1].second, true)
             }
         }
-        l.filter { it.second xor  it.third }.forEach { t ->
-            clauses.removeAll { it.element.contains(t.first) || it.element.contains(-t.first) }
-            if (t.second) literal[t.first - 1] = Triple(t.first, second = true, third = true)
-            else literal[t.first - 1] = Triple(t.first, second = false, third = true)
+        l.filter { it.second xor it.third }.forEach { t ->
+            cnf.clauses.removeAll { it.element.contains(t.first) || it.element.contains(-t.first) }
+            if (t.second) cnf.literal[t.first - 1] = Triple(t.first, second = true, third = true)
+            else cnf.literal[t.first - 1] = Triple(t.first, second = false, third = true)
         }
 
-        return this
+        return cnf
     }
 }
